9a996a6209ab8fc0f8743f5c5fe636eebc9b79d8
Eisa Khan Becoming Stack Developer <119102838+eisakhan762@users.noreply.github.com>
2024-08-12 20:04:49 +0530

    Delete Python Course With Notes/Chapter 01 - Modules, Comments and PIP directory

--- Python Course With Notes/Chapter 01 - Modules, Comments and PIP/02_pro_01_twinkle.py
+++
@@ -1,9 +0,0 @@
-# All Credit Goes To Code With Harry
-# Actual Course Link -> https://youtu.be/gfDE2a7MKjA?si=bBPBJ4qqKqEUBn02
-'''
-This Is a solution of Problem 01 One of Code With Harry Practice Set 1!
-'''
-print('''Twilkle Twinkle Little Start
-How I Wornder What You
-Up Above The World So High
-Like A Heera In The Sky.''')
