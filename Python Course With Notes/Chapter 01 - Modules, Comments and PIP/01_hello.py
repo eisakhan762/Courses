8cc92e361713738a03cdc6fbf4f94c066586c79c
Eisa Khan Becoming Stack Developer <119102838+eisakhan762@users.noreply.github.com>
2024-08-12 19:50:44 +0530

    Add files via upload

---
+++ Python Course With Notes/Chapter 01 - Modules, Comments and PIP/01_hello.py
@@ -0,0 +1,10 @@
+import os
+import os
+# sit
+'''
+dsfjasifhjdshfjn
+djfdkjfsad
+dfjdhfjds
+fdfdadsjfs'''
+print("Hello world")
+print("Twinkle Twinkle little star.")
---
+++ Python Course With Notes/Chapter 01 - Modules, Comments and PIP/02_pro_01_twinkle.py
@@ -0,0 +1,7 @@
+'''
+This Is a solution of Problem 01 One of Code With Harry Practice Set 1!
+'''
+print('''Twilkle Twinkle Little Start
+How I Wornder What You
+Up Above The World So High
+Like A Heera In The Sky.''')
